14d339844d3672305b2fc9821af7e9d7a5b6c4c9
narbaysebas@gmail.com <narbaysebas@gmail.com>
2021-03-16 12:32:01 -0300

    Migrated MassTransit & RabbitMQ to Play.Common To reuse in our microservices

---
+++ Play.Common/src/Play.Common/MassTransit/Extensions.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using MassTransit;
+using MassTransit.Definition;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Play.Common.Settings;
+
+namespace Play.Common.MassTransit
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMassTransitWithRabbitMq(this IServiceCollection services)
+        {
+            services.AddMassTransit(configure => 
+            {
+                configure.AddConsumers(Assembly.GetEntryAssembly()); // any consumer class
+
+                configure.UsingRabbitMq((context, configurator) => 
+                {
+                    var config = context.GetService<IConfiguration>();
+                    var settings = config.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                    var rabbitMqSettings = config.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+                    configurator.Host(rabbitMqSettings.Host);
+                    configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(settings.ServiceName, false));
+                });
+            });
+            //Starts the RabbitMQ bus
+            services.AddMassTransitHostedService();
+
+            return services;
+        }
+    }
+}
